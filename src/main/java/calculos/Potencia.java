4ca7b30ba1a450327e92a72e1178f029b64bcbd2
mgbosque <marta.g.bosque@gmail.com>
2026-03-18 08:28:57 +0100

    Primer commit

---
+++ src/main/java/calculos/Potencia.java
@@ -0,0 +1,9 @@
+package calculos;
+
+
+public class Potencia {
+    public static double calcularPotencia(double base, double exponente) {
+        double resultado = Math.pow(base, exponente);
+        return resultado;
+}
+}
